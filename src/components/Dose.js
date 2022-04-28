53451183ba1eb95ce6507cfdb2408c078c46ff66
Ann Vorobyeva <a.vorobyeva@itransition.com>
2022-04-28 19:53:07 +0300

    design upd

--- src/components/Dose.js
+++ src/components/Dose.js
@@ -3,7 +3,7 @@ import { randomDose } from '../utils/random.js'
 import '../styles/dose.css'
 
 const Dose = () => {
-    const title = 'How much drinks will you get';
+    const title = 'Drink random attraction';
     const [dose, setDose] = useState(0);
 
     const showDose = (event) => {
@@ -14,10 +14,11 @@ const Dose = () => {
     return (
         <div className='dose'>
         <h1 className='dose__title'>{title}</h1>
+        <p className='dose__desc'>Can't decide how much drinks do you want? <br /> Enter a number and get decision.</p>
         <form className='dose__form' onSubmit={showDose}>
-            <input className='dose__input' placeholder='Start from...' type="number"></input>
-            <div>{dose} {dose <= 1 ? 'drink' : 'drinks'}</div>
-            <button className='dose__btn'>Get</button>
+            <input className='dose__input' placeholder='How much drinks do you want?' type="number"></input>
+            <div>You should drink <strong>{dose}</strong> {dose <= 1 ? 'drink' : 'drinks'}</div>
+            <button className='dose__btn'>Decide</button>
         </form>
 
         </div>
